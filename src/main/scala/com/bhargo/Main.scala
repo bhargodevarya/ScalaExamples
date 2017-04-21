920301b9bbdcaba0e6db6243d3820ac3d44ace5b
Bhargo <bhargodevarya@gmail.com>
2017-04-21 21:34:05 +0530

    update .gitignore, companion object, constructor overloading, getter & setters, val, var, inline code

--- src/main/scala/com/bhargo/Main.scala
+++ src/main/scala/com/bhargo/Main.scala
@@ -1,10 +1,15 @@
 package com.bhargo
 
+import com.bhargo.model.Employee
+
 /**
   * Created by barya on 4/17/2017.
   */
 object Main {
 
-  def main(args:Array[String]) = println("Hello!!!")
-
+  def main(args:Array[String]) = {
+    val emp:Employee = new Employee("41NYG", "Bhargodev Arya", "Bhargodev.arya@morganstanley.com")
+    emp.name = "Amar"
+    println(emp)
+  }
 }
